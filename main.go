fe695d9d6b66bfeff13e4a8ca574f994cb4749f3
koh-sh <34917718+koh-sh@users.noreply.github.com>
2023-08-05 21:53:36 +0900

    Revert "feature: set version from goreleaser"

--- main.go
+++ main.go
@@ -2,13 +2,6 @@ package main
 
 import "github.com/koh-sh/codebuild-multirunner/cmd"
 
-var (
-	version = "dev"
-	commit  = "none"
-	date    = "unknown"
-)
-
 func main() {
-	cmd.SetVersionInfo(version, commit, date)
 	cmd.Execute()
 }
